b2fdb492e71c1a96d73dc09e16e127001c3a0515
aas4cont-web <aas4cont@gmail.com>
2026-07-07 10:29:05 +0530

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+// jest.config.js
+module.exports = {
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/index.js',
+    '!src/**/*.test.js',
+  ],
+  testMatch: ['**/__tests__/**/*.test.js'],
+};
